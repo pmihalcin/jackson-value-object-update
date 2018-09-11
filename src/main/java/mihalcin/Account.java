e1262e6ac26c9ed9a55dd47d9cdb8162eb76f6c5
HCG\Patrik.Mihalcin <patrik.mihalcin@embedit.cz>
2018-09-11 11:42:23 +0200

    Don't use deep merge

--- src/main/java/mihalcin/Account.java
+++ src/main/java/mihalcin/Account.java
@@ -1,14 +1,11 @@
 package mihalcin;
 
-import static com.fasterxml.jackson.annotation.OptBoolean.TRUE;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonMerge;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Account {
 
-    @JsonMerge(value = TRUE)
+    //    @JsonMerge(value = TRUE)
     private final Validity validity;
 
     @JsonCreator
